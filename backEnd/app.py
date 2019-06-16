3bc0d1248ef490bc13e3d592106dc235c38eb4e8
Lori Bailey <>
2019-06-16 18:09:17 +0100

    Created sockets to listen for changes in reservation status and update app.

--- backEnd/app.py
+++ backEnd/app.py
@@ -1,27 +1,29 @@
-from flask import Flask, flash, jsonify, render_template, request, session, abort
-app = Flask(__name__)
-
-
-from pymongo import MongoClient
-from bson.objectid import ObjectId
-from pprint import pprint
-import os
-import time
-from flask_socketio import SocketIO, emit
-from collections import deque
-
-from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from collections import deque
+from flask_socketio import SocketIO, emit
+import time
+import os
+from pprint import pprint
+from bson.objectid import ObjectId
+from pymongo import MongoClient
+from flask import Flask, flash, jsonify, render_template, request, session, abort
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+socketio = SocketIO(app)
 
 client = MongoClient("mongodb://localhost:27017")
-db=client.reddot
+db = client["red-dot-art"]
 
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+
 @app.route("/")
 def home():
-    serverStatusResult=db.command("serverStatus")
+    serverStatusResult = db.command("serverStatus")
     pprint(serverStatusResult)
     return "Hello, Flask!"
 
@@ -33,12 +35,13 @@ def profileData():
 
     # Request profiles from database, stored in Cursor object
     artistsProfilesCursor = db.artistsProfiles.find()
-    
+
     # Adds each profile to the artistsProfiles array
     for profile in artistsProfilesCursor:
-        addProfile = {"_id": str(profile["_id"]), "image": profile["image"], "name": profile["name"], "programme": profile["programme"], "youtube": profile["youtube"], "instagram": profile["instagram"], "facebook": profile["facebook"], "web": profile["web"]}
+        addProfile = {"id": str(profile["_id"]), "image": profile["image"], "name": profile["name"], "programme": profile["programme"],
+                      "youtube": profile["youtube"], "instagram": profile["instagram"], "facebook": profile["facebook"], "web": profile["web"]}
         artistsProfiles.append(addProfile)
-    
+
     # Returns a jsonified object of artists profiles
     return jsonify(artistsProfiles)
 
@@ -50,49 +53,51 @@ def vipDonors():
 
     # Request donor profiles from database, stored in Cursor object
     vipDonorsCursor = db.vipDonors.find()
-    
+
     # Adds each donor to the donors array
     for donor in vipDonorsCursor:
-        addDonor = {"_id": str(vipDonors["_id"]), "name": vipDonors["name"], "image": donor["image"]}
+        addDonor = {"id": str(
+            donor["_id"]), "name": donor["name"], "image": donor["image"]}
         vipDonors.append(addDonor)
-    
+
     # Returns a jsonified object of artists profiles
     return jsonify(vipDonors)
 
 
 # Retrieves art work data
 @app.route("/artworks", methods=["GET"])
-def artworks():
+def artWorks():
     # Empty array for artwork data
-    artworks = []
+    artWorks = []
 
-    # Request artwork profiles from database, stored in Cursor object
-    artworksCursor = db.artworks.find()
-    
-    # Adds each donor to the donors array
-    for artworks in artworksCursor:
-        addWork = {"_id": str(artworks["_id"]), "image": artworks["image"], "title": work["title"], "artistid": work["artistid"], "description": work["description"], "buyerid": work["buyerid"], "price": float(str(work["price"])), "sponsor": work["sponsor"], "reddotstatus": work["reddotstatus"]}
-        artworks.append(addWork)
-    
+    # Request artworks  from database, stored in Cursor object
+    artWorksCursor = db.artWorks.find()
+
+    # Adds each work to the artWorks array
+    for work in artWorksCursor:
+        addWork = {"id": str(work["_id"]), "image": work["image"], "title": work["title"], "artistid": work["artistid"], "description": work["description"],
+                   "buyerid": work["buyerid"], "price": float(str(work["price"])), "sponsor": work["sponsor"], "reddotstatus": work["reddotstatus"]}
+        artWorks.append(addWork)
+
     # Returns a jsonified object of artists profiles
-    return jsonify(artworks)
+    return jsonify(artWorks)
 
 # Handles reservations
-@app.route("/reservation", methods=["GET", "POST"])
+@app.route("/reservation", methods=["POST"])
 def makeReservation():
     if request.method == "POST":
+        reservationObject = request.get_json()
 
-        reservationObject= request.get_json()
-
-        db.artworks.find_one_and_update(
-            {"_id" : ObjectId(reservationObject["workId"])},
+        # Updates the database, recording the donor as the buyer, and changing status to reserved.
+        db.artWorks.find_one_and_update(
+            {"_id": ObjectId(reservationObject["workId"])},
             {"$set": {
-                       "buyerid" : reservationObject["donorId"],
-                       "reddotstatus": "Reserved"}})
+                "buyerid": reservationObject["donorId"],
+                "reddotstatus": "Reserved"}})
 
- 
-        #socketio.emit("reserved", channel, broadcast=True)
-
+        # Updates reddotstatus on the client side of the work identified in the reservationObject
+        socketio.emit(
+            "Reserved", {"id": reservationObject["workId"]}, broadcast=True)
 
         return ""
     else:
--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -1,6 +1,11 @@
 // From 3rd party libraries
 import React, { Component } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
+import io from "socket.io-client";
+import _ from "lodash";
+
+// Context to be used in other components
+import { StoreProvider } from "./storeContext";
 
 // Compotents from this application
 import DotMenu from "./components/dotMenu";
@@ -14,45 +19,49 @@ import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 import axios from "axios";
 
+var socket = io.connect("http://10.233.1.169:5000/");
+
 class App extends Component {
   state = { artistsProfiles: [], artWorks: [], vipDonors: [] };
 
-  componentDidMount() {
+  async componentDidMount() {
     // Retrieves data and stores in state to be accessed as props by other components
 
-    axios.get("http://127.0.0.1:5000/artistsprofiles").then(res => {
-      const artistsProfiles = res.data;
-      this.setState({ artistsProfiles });
-    });
+    let res = await axios.get("http://10.233.1.169:5000/artistsprofiles");
+    const artistsProfiles = res.data;
 
-    axios.get("http://127.0.0.1:5000/artworks").then(res => {
-      const artWorks = res.data;
-      this.setState({ artWorks });
-    });
+    res = await axios.get("http://10.233.1.169:5000/artworks");
+    const artWorks = res.data;
 
-    axios.get("http://127.0.0.1:5000/vipdonors").then(res => {
-      const vipDonors = res.data;
-      this.setState({ vipDonors });
-    });
+    res = await axios.get("http://10.233.1.169:5000/vipdonors");
+    const vipDonors = res.data;
+    this.setState({ artistsProfiles, artWorks, vipDonors });
+
+    socket.on("Reserved", this.handleReservation);
   }
 
+  handleReservation = event => {
+    const newArtWorks = _.map(this.state.artWorks, work => {
+      if (work.id === event.id) {
+        work.reddotstatus = "Reserved";
+      }
+      return work;
+    });
+    this.setState({ artWorks: newArtWorks });
+  };
+
   render() {
-    const { artistsProfiles, artWorks, vipDonors } = this.state;
     return (
       <React.Fragment>
         <Switch>
-          <Route
-            path="/catalogue"
-            component={Catalogue}
-            artWorks={artWorks}
-            vipDonors={vipDonors}
-            artistsProfiles={artistsProfiles}
-          />
-          <Route path="/confirmation" component={Reserve} />
-          <Route path="/work/:id" component={Tabs} vipDonors={vipDonors} />
-          <Redirect path="/work" exact to="/catalogue" />
-          <Route path="/not-found" component={NotFound} />
           <Redirect from="/" exact to="/catalogue" />
+          <Redirect path="/work" exact to="/catalogue" />
+          <StoreProvider value={this.state}>
+            <Route path="/catalogue" component={Catalogue} />
+            <Route path="/confirmation" component={Reserve} />
+            <Route path="/work/:id" component={Tabs} />
+          </StoreProvider>
+          <Route path="/not-found" component={NotFound} />
           <Redirect to="/not-found" />
         </Switch>
         <DotMenu />
